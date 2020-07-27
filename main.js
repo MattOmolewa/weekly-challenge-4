0cb60ae18d20042c30a954b8f34d01bf0fe56730
mattomolewa <matthew.omolewa@gmail.com>
2020-07-27 11:53:02 +0100

    added details

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+/* A construction company is building a new neighborhood, 
+and they are currently working on the design. 
+Each house will be built using one of three main materials 
+(e.g., wood, brick, or concrete), but no side-by-side houses can be 
+made of the same material. Because each house will be of varying size 
+and complexity, the cost of the materials for each house varies. 
+Given the cost of using each material for each house, what is the 
+minimum cost needed to complete the neighborhood?
+
+YOUR TASK
+
+Write a function minCost which:
+
+1. has a parameter cost:  a 2-dimensional array of integers where 
+cost[i][j] denotes the cost of using the jth material on the ith house, and
+
+2. returns minimumCost (an integer which is the minimum cost to 
+build all the houses in the neighborhood)
+
+*/
+
+
+
 function minCost(costs) {
     if(costs==null||costs.length==0)
         return 0;
